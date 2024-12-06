383443edc8b27b932faa87211b538a78ab359546
abel-git <camilosol123@gmail.com>
2024-12-06 15:52:24 -0500

    🎉 init:

--- docs/script copy.js
+++ docs/script copy.js
@@ -4,7 +4,7 @@ class AWaves extends HTMLElement {
    */
   connectedCallback() {
     // Elements
-    this.canvas = this.querySelector(".js-canvas");
+    this.canvas = tahis.querySelector(".js-canvas");
     this.ctx = this.canvas.getContext("2d");
 
     // Properties
@@ -208,7 +208,7 @@ class AWaves extends HTMLElement {
     ctx.clearRect(0, 0, bounding.width, bounding.height);
 
     ctx.beginPath();
-    ctx.strokeStyle = "#2a3f34";
+    ctx.strokeStyle = "black";
 
     lines.forEach((points, lIndex) => {
       let p1 = moved(points[0], false);
@@ -270,104 +270,3 @@ class AWaves extends HTMLElement {
 }
 
 customElements.define("a-waves", AWaves);
-
-// Menú activo
-const lista = document.getElementById('list_item');
-lista.addEventListener('click', function(event) {
-
-  if (event.target.tagName === 'LI') {
-    // Remover la clase active
-    const items = lista.getElementsByTagName('li');
-    for (let item of items) {
-      item.classList.remove('active');
-    }
-    // Agregar la clase active
-    event.target.classList.add('active');
-    
-    // Obtener el value de los <li> clickeado
-    const selectedValue = event.target.getAttribute('value');
-    
-    // Cambiar la cara visible del swiper en base al value
-    swiper.slideTo(getSlideIndexByValue(selectedValue), 1000);
-  }
-});
-
-// Función para obtener el índice del slide en base al value
-
-function getSlideIndexByValue(value) {
-  const slideIds = {
-    "senatic": 0,
-    "TalentoTech": 1,
-    "AvanzaTec": 2,
-    "Gobiernodigital": 3,
-    "Yawa": 4
-  };
-  return slideIds[value] || 0;  // Valor predeterminado si no se encuentra
-}
-
-// Swiper (cubo)
-var swiper = new Swiper(".swiper", {
-  effect: "cube",
-  grabCursor: true,
-  loop: true,
-  speed: 1000,
-  cubeEffect: {
-    shadow: false,
-    slideShadows: true,
-    shadowOffset: 10,
-    shadowScale: 0.94,
-  },
-  // autoplay: {
-  //   delay: 2600,
-  //   pauseOnMouseEnter: true,
-  // },
-});
-
-// Inicializa el fondo de partículas (si es necesario)
-tsParticles.load("tsparticles", {
-  fpsLimit: 60,
-  backgroundMode: {
-    enable: true,
-    zIndex: -1,
-  },
-  particles: {
-    number: {
-      value: 30,
-      density: {
-        enable: true,
-        area: 800,
-      },
-    },
-    color: {
-      value: [
-        "#3998D0",
-        "#2EB6AF",
-        "#A9BD33",
-        "#FEC73B",
-        "#F89930",
-        "#F45623",
-        "#D62E32",
-      ],
-    },
-    shape: {
-      type: "circle",
-      stroke: {
-        width: 0,
-        color: "#000000",
-      },
-    },
-    size: {
-      value: 8,
-      random: {
-        enable: true,
-        minimumValue: 4,
-      },
-    },
-    move: {
-      enable: true,
-      speed: 7,
-      direction: "none",
-    },
-  },
-  detectRetina: true,
-});
